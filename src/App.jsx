0c2102416e1be37b5614739ad9e64bc5a45516e5
Min Paing <minmyatpaing64@gmail.com>
2024-11-16 19:22:14 +0000

    completed navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
-import './App.css'
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Navbar,
+  Hero,
+  Works,
+  Experience,
+  Tech,
+  Feedbacks,
+  Contact,
+  StarsCanvas,
+} from "./components";
 
 function App() {
-
   return (
-    <div>3d react app</div>
-  )
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from "../styles";
+import { navLinks } from "../constants/index";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [active, setActive] = useState("");
+  const [openMenu, setOpenMenu] = useState(false);
 
-export default Navbar
+  return (
+    <nav
+      className={`${styles.paddingX} flex w-full bg-primary py-5 fixed top-0 z-20 items-center`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to={"/"}
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">Min</p>
+        </Link>
+
+        <ul className="list-none hidden sm:flex flex-row items-center gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={openMenu ? close : menu}
+            alt="menu button"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setOpenMenu((preState) => !preState)}
+          />
+
+          <div
+            className={`${
+              !openMenu ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className="list-none flex flex-col justify-start items-center gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  } font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick={() => {
+                    setOpenMenu(false);
+                    setActive(link.title);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
